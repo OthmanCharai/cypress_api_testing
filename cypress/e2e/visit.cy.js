10107d0a483c84627cda34cf79f45ae46b47271b
OthmanCharai <otmanchar3i0688@gmail.com>
2022-08-27 20:51:57 +0100

    assert 200

--- cypress/e2e/visit.cy.js
+++ cypress/e2e/visit.cy.js
@@ -1,10 +1,24 @@
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    cy.visit("http://lokisalle.quentin-vilfeu.com/");
-  });
-});
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(false);
-  });
+describe(" inach backend testing", () => {
+  let i;
+  let flager_id=1;
+  let action_id=1;
+  
+  let fetch_web_hook_url="fetch-web-hook/"+action_id+"/flagger/"+flager_id
+  let bash_info_url="queue/info";
+
+  it("fetches Todo items - GET", () => {
+    for (i = 0; i <100; i++) {
+      cy.request('POST',fetch_web_hook_url,{post_id:'1'}).as("queueRequest");
+      cy.get("@queueRequest").then((todos) => {
+        expect(todos.status).to.eq(200);
+        assert.isArray(todos.body, "Response it's 200");
+      });
+      
+    }
+    cy.request('GET',bash_info_url).as("todoRequest");
+    cy.get("@todoRequest").then((todos) => {
+      expect(todos.status).to.eq(200);
+      assert.isArray(todos.body, "Response it's 200");
+    });
+    })
 });
