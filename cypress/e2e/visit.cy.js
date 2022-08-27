310b02b36055122a6a4673da1a674cf6d2b62963
OthmanCharai <otmanchar3i0688@gmail.com>
2022-08-27 11:44:35 +0100

    Cypress Testing

---
+++ cypress/e2e/visit.cy.js
@@ -0,0 +1,10 @@
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    cy.visit("http://lokisalle.quentin-vilfeu.com/");
+  });
+});
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(false);
+  });
+});
