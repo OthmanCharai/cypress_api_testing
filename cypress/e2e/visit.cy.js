d99c66b281efc956baf633f6ef327e449125bd4e
OthmanCharai <otmanchara3i0688@gmail.com>
2022-09-07 11:56:32 +0100

    add life hook

--- cypress/e2e/visit.cy.js
+++ cypress/e2e/visit.cy.js
@@ -1,22 +1,43 @@
 describe(" inach backend testing", () => {
   let i;
-  let flager_id = 1;
-  let action_id = 202;
-
-  let fetch_web_hook_url =
-    "fetch-web-hook/" + action_id + "/flagger/" + flager_id;
+  let action_id = 101;
+  let token_api='';
+  let fetch_web_hook_url ='';
+  let login_url =
+  "user/login";
   let bash_info_url = "queue/info";
-
+  before(() => {
+    cy.request({
+      method: "POST",
+      url: login_url,
+      body: { 
+        'email':Cypress.env('email'),
+        'password':Cypress.env('password')
+       },
+       headers: {
+        Accept:
+          " application/json",
+          Content :' application/json'
+      }, 
+    }).as("queueRequest");
+    cy.get("@queueRequest").then((todos) => {
+      token_api=todos.body.token;
+    });
+  })
+  
+  
   it("fetches Todo items - GET", () => {
-    for (i = 0; i < 100; i++) {
-     cy.wait(1000) // wait for 2 seconds
-
+    for (i = 1; i <= 100; i++) {
+      cy.wait(1000) // wait for 2 seconds
+      fetch_web_hook_url =
+      "fetch-web-hook/" + action_id + "/flagger/" + i;
+      //alert(token_api)
       cy.request({
         method: "POST",
         url: fetch_web_hook_url,
         body: { post_id: "1" },
         headers: {
-          Authorization: "Bearer" + Cypress.env("token"),
+          Authorization: "Bearer " + token_api,
         },
       }).as("queueRequest");
       cy.get("@queueRequest").then((todos) => {
@@ -24,12 +45,12 @@ describe(" inach backend testing", () => {
         console.log(todos.body.Message);
       });
     }
-    cy.wait(2000) // wait for 2 seconds
+    cy.wait(6000) // wait for 2 seconds
     
     cy.request({
       url: bash_info_url,
       headers: {
-        Authorization: "Bearer" + Cypress.env("token"),
+        Authorization: "Bearer " + token_api,
       },
     }).as("todoRequest");
 
